272ebf54b58479fccc540b1766f3baf45c3bb2f0
Sadik Kilkis <sadik.kk@hotmail.com>
2026-04-20 22:23:46 +0300

    vessel_edit: kaydet sonrası ilgili segment aç

--- app.py
+++ app.py
@@ -446,7 +446,7 @@ def vessel_edit(vessel_id):
             vessel_id
         ))
         db.commit()
-        return redirect(url_for('vessels'))
+        return redirect(url_for('vessels') + '?seg=' + request.form.get('durum', 'gelecek'))
     return render_template('vessel_edit.html', vessel=vessel)
 
 
