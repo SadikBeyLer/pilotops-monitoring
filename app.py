0e1885b064a74cb2623a4fccc3b6cd1719673f2d
Sadik Kilkis <sadik.kk@hotmail.com>
2026-04-24 12:35:58 +0300

    feat: edit saat sirasi kontrolu eklendi

--- app.py
+++ app.py
@@ -668,6 +668,13 @@ def operation_edit(op_id):
     if prev_bitis:
         if datetime.fromisoformat(off_st) < datetime.fromisoformat(prev_bitis['on_station']):
             return 'Off Station önceki işin bitiş saatinden (' + prev_bitis['on_station'][11:16] + ') önce olamaz', 400
+    # Saat sırası kontrolü
+        if pob and datetime.fromisoformat(pob) < datetime.fromisoformat(off_st):
+            return 'POB, Off Station\'dan önce olamaz', 400
+        if poff and pob and datetime.fromisoformat(poff) < datetime.fromisoformat(pob):
+            return 'P.Off, POB\'dan önce olamaz', 400
+        if on_st and poff and datetime.fromisoformat(on_st) < datetime.fromisoformat(poff):
+            return 'On Station, P.Off\'dan önce olamaz', 400
     is_tipi, k = detect_is_tipi(from_nokta, to_nokta)
 
     if off_st and pob and poff and on_st:
